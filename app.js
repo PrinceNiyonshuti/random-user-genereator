8bf8b48132a2ee8c08133cfd077f93c7d71de020
PrinceNiyonshuti <npprince47@gmail.com>
2022-01-14 20:26:05 +0200

    handling API data and render data

--- app.js
+++ app.js
@@ -10,12 +10,16 @@ const app = Vue.createApp({
 		};
 	},
 	methods: {
-		getUser() {
-			this.firstName = 'Lucifer'
-			this.lastName = 'Morning Star'
-			this.email = "Lucifer@star.com"
-			this.gender = 'male'
-			this.picture = "https://randomuser.me/api/portraits/men/10.jpg"
+		async getUser() {
+			const res = await fetch("https://randomuser.me/api");
+			const {results} = await res.json();
+			console.log(results);
+
+			this.firstName = results[0].name.first;
+			this.lastName = results[0].name.last;
+			this.email = results[0].email;
+			this.gender = results[0].gender;
+			this.picture = results[0].picture.large;
 		},
 	},
 });
